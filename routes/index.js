8ba8ed28befe85549df774c5e2389906a1c7077e
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2018-02-09 16:16:07 +0530

    Top Content Dynamic Update

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var videos = [
+  {
+    url:"https://d3d0fl6l6djog.cloudfront.net/IPL-2017/SUNRISERSVSROYALCHALLENGERS050417/SUNRISERSVSROYALCHALLENGERS050417-SH-Inn1-Over1-Ball2.mp4",
+    title:"Ind vs SA - SA tour virat 50",
+    length:"4:58",
+    date:"Oct - 10"
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.json(videos);
+});
+
+
+router.get('/videos',function(req,res,next) {
+  res.json(videos);
+});
+
+
+module.exports = router;
